d131778cbfa9aa713f21e0f5b780ffc92602401c
Martin <mjuliom_4@hotmail.com>
2022-10-19 20:48:40 -0300

    Swagger and swagger ui

--- src/main/java/com/example/tenpo/security/WebSecurity.java
+++ src/main/java/com/example/tenpo/security/WebSecurity.java
@@ -3,12 +3,15 @@ package com.example.tenpo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,6 +22,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter  {
 
 
 
+/*
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
@@ -26,6 +30,21 @@ public class WebSecurity extends WebSecurityConfigurerAdapter  {
 				.antMatchers("/h2-console/**").permitAll();
 		http.csrf().disable();
 		http.headers().frameOptions().disable();
+	}
+*/
+
+
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable()
+				.authorizeRequests()
+				.antMatchers( "/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**","/swagger-resources/configuration/ui","/swagger-ui.html","/h2-console/**")
+				.permitAll();
+				/*.antMatchers("/api/users/login").permitAll()
+				.antMatchers(HttpMethod.POST, "/api/users").permitAll();*/
+		http.headers().frameOptions().disable();
+
 	}
 
 
